b558cc3dc28de8819ae8e74f4c05926cc16edf05
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-15 21:18:14 +0300

    feat: added ability to add extra stuff after each emojie
    usefull for other plugins depending on it

--- src/main/java/com/lith/emojies/util/EmojiesUtil.java
+++ src/main/java/com/lith/emojies/util/EmojiesUtil.java
@@ -7,8 +7,12 @@ import com.lith.emojies.Plugin;
 
 public class EmojiesUtil {
     public static String addEmojies(@NotNull String text) {
+        return addEmojies(text, "");
+    }
+
+    public static String addEmojies(@NotNull String text, @NotNull String extra) {
         for (Map.Entry<Pattern, String> entry : Plugin.plugin.configs.getEmojies().entrySet())
-            text = entry.getKey().matcher(text).replaceAll(entry.getValue());
+            text = entry.getKey().matcher(text).replaceAll(entry.getValue() + extra);
 
         return text.trim();
     }
